a6838ec1c5266ebae5f7d183b692eb92f51d37a0
Git-Santhosh <https://github.com/Git-Santhosh/MySample.git>
2020-02-16 11:19:32 +0530

    implementing  mainView Design part

---
+++ workmateTask/UtilityGroup/ButtonUtility.swift
@@ -0,0 +1,9 @@
+//
+//  ButtonUtility.swift
+//  workmateTask
+//
+//  Created by Ducont on 16/02/20.
+//  Copyright © 2020 Ducont. All rights reserved.
+//
+
+
---
+++ workmateTask/UtilityGroup/ColorUtility.swift
@@ -0,0 +1,9 @@
+//
+//  ColorUtility.swift
+//  workmateTask
+//
+//  Created by Ducont on 16/02/20.
+//  Copyright © 2020 Ducont. All rights reserved.
+//
+
+import Foundation
